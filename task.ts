3488dedc6902099e5fe85680b4c3cd22bade3a36
my name <biba@lol.ru>
2022-09-20 14:43:44 +0800

    export + import

--- task.ts
+++ task.ts
@@ -1,17 +1,3 @@
+import { getTimeBeforeNY } from "./lib";
 let time = getTimeBeforeNY(new Date());
 console.log("До нового года осталось " + time[0] + " дней, " + time[1] + " часов и " + time[2] + " минут!");
-
-function getTimeBeforeNY(date: Date) : number[]{
-    date.setHours(date.getHours()+8)//UTF+8))
-    let newYearDate = new Date(0);
-    newYearDate.setFullYear(date.getFullYear()+1);
-    
-    let timeMillis = newYearDate.getTime() - date.getTime();
-    
-    
-    let days = Math.floor(timeMillis/(1000*60*60*24))
-    let hours = Math.floor((timeMillis - days*1000*60*60*24)/(1000*60*60));
-    let minutes = Math.floor((timeMillis - days*1000*60*60*24 - hours*1000*60*60)/(1000*60));
-
-    return [days, hours, minutes];
-}
