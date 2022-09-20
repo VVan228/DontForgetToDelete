f5c137dc456231256f9561847368d54764b539e5
my name <biba@lol.ru>
2022-09-20 14:13:07 +0800

    js->ts

--- task.ts
+++ task.ts
@@ -1,12 +1,18 @@
-let date = new Date();
-date.setHours(date.getHours()+8)//UTS+8))
-let newYearDate = new Date(0);
-newYearDate.setFullYear(date.getFullYear()+1);
+let time = getTimeBeforeNY();
+console.log("До нового года осталось " + time[0] + " дней, " + time[1] + " часов и " + time[2] + " минут!");
 
-let timeMillis = newYearDate.getTime() - date.getTime();
+function getTimeBeforeNY(){
+    let date = new Date();
+    date.setHours(date.getHours()+8)//UTF+8))
+    let newYearDate = new Date(0);
+    newYearDate.setFullYear(date.getFullYear()+1);
+    
+    let timeMillis = newYearDate.getTime() - date.getTime();
+    
+    
+    let days = Math.floor(timeMillis/(1000*60*60*24))
+    let hours = Math.floor((timeMillis - days*1000*60*60*24)/(1000*60*60));
+    let minutes = Math.floor((timeMillis - days*1000*60*60*24 - hours*1000*60*60)/(1000*60));
 
-
-let days = parseInt(timeMillis/(1000*60*60*24))
-let hours = parseInt((timeMillis - days*1000*60*60*24)/(1000*60*60));
-let minutes = parseInt((timeMillis - days*1000*60*60*24 - hours*1000*60*60)/(1000*60));
-console.log("До нового года осталось " + days + " дней, " + hours + " часов и " + minutes + " минут!");
+    return [days, hours, minutes];
+}
